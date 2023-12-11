7f5173a4466fff6223f91b15afac80d94227e5c3
Enzo Viry <enzo.viry@gmail.com>
2023-12-11 18:13:59 +0100

    fix: image

--- src/components/Section4/Section4.tsx
+++ src/components/Section4/Section4.tsx
@@ -3,12 +3,13 @@ import parse from 'html-react-parser';
 
 
 function Section4({data}: { data: Section4 }) {
+    console.log(data)
     return (
         <section className={"section_4"} id={"section_4"}>
             <h2 className={"section_4__title"}>{data.titre}</h2>
             <img
-                src={'/images/section2.png'}
-                alt={""}
+                src={data.image.url}
+                alt={data.image.alt}
                 className={"section_4__image"}
             />
             <div>
